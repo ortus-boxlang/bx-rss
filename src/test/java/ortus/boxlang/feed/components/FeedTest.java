d4fa64483b5e9f8262edcfaf7adea469d932566c
Luis Majano <lmajano@gmail.com>
2025-10-29 15:53:55 +0100

    feed debugging

--- src/test/java/ortus/boxlang/feed/components/FeedTest.java
+++ src/test/java/ortus/boxlang/feed/components/FeedTest.java
@@ -1,46 +1,7 @@
 package ortus.boxlang.feed.components;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import ortus.boxlang.feed.BaseIntegrationTest;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import ortus.boxlang.compiler.parser.BoxSourceType;
-import ortus.boxlang.runtime.BoxRuntime;
-import ortus.boxlang.runtime.context.IBoxContext;
-import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
-import ortus.boxlang.runtime.scopes.IScope;
-import ortus.boxlang.runtime.scopes.Key;
-import ortus.boxlang.runtime.scopes.VariablesScope;
-
-public class FeedTest {
-
-	static BoxRuntime	instance;
-	IBoxContext			context;
-	IScope				variables;
-	static Key			result	= new Key( "result" );
-
-	@BeforeAll
-	public static void setUp() {
-		instance = BoxRuntime.getInstance( true );
-	}
-
-	@BeforeEach
-	public void setupEach() {
-		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
-		variables	= context.getScopeNearby( VariablesScope.name );
-	}
-
-	@DisplayName( "It can test the ExampleComponent" )
-	@Test
-	public void testExampleComponent() {
-		instance.executeSource( """
-		                        	<cfExampleComponent name="Ortus Solutions">
-		                        	<cfset result = getBoxContext().getBuffer().toString()>
-		                        """, context, BoxSourceType.CFTEMPLATE );
-		assertTrue( variables.getAsString( result ).contains( "Hello, world - from Ortus Solutions." ) );
-	}
+public class FeedTest extends BaseIntegrationTest {
 
 }
